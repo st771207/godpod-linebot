04c239eb1ceb92649dd7d34cf203e27192c43cc9
st771207 <st771207@gmail.com>
2025-07-07 17:30:44 +0800

    Create .render-build.sh

---
+++ .render-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo ">>> Uninstalling existing line-bot-sdk..."
+pip uninstall -y line-bot-sdk
+
+echo ">>> Installing line-bot-sdk version 3.7.0..."
+pip install line-bot-sdk==3.7.0
+
+
+
+Add .render-build.sh to force correct SDK version
